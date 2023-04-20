bc92ebcf2110fb20ac21fefc6b0e809338078a58
Jose Gustavo Valladares Barahona <gusbarahona01@gmail.com>
2023-04-19 23:37:03 -0600

    Arreglando background Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,15 +4,19 @@ import Footer from "../Componentes/Footer"
 import GlobalEstilos from "../Global"
 import Videos from "../Componentes/Carrusel"
 import VideoCard from "../Componentes/VideoCard"
+import styled from "styled-components"
 
 function Home() {
+  const FondoHome = styled.div`
+      background-color:#000000;
+      `;
   return (
-    <>
+    <FondoHome>
      <GlobalEstilos />
       <Banner />
       <Videos />
       <Footer />
-    </>
+    </FondoHome>
   );
 }
 
